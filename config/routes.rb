020ef125e396182c367db95896a376ebbc4353d4
Mack Earnhardt <mack@agilereasoning.com>
2011-04-04 10:39:18 -0400

    Apply patches

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,24 @@ HouseCloser::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :roles
+  resources :users do
+    resource :password, :only => [:create, :edit, :update]
+  end
+  
+  resources :passwords, :only => [:new, :create]
+  resource :session, :only => [:new, :create, :destroy]
+
+  match 'sign_up'   => 'accounts#new'
+  match 'sign_in'   => 'sessions#new'
+  match 'sign_out'  => 'sessions#destroy', :via => :delete
+
+  resource :account do
+    resources :confirmations, :only => [:new, :create, :edit]
+  end
+
+  root :to => "accounts#new"
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -9,7 +9,7 @@ module NavigationHelpers
     case page_name
 
     when /the home\s?page/
-      '/'
+      root_path
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
@@ -17,6 +17,16 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+    when /my password reset page/i
+      account_password_reset_path(User.last.confirmation_token)
+    when /my edit password reset page/i
+      edit_account_password_reset_path(User.last.confirmation_token)
+    when /an invalid edit password reset page/i
+      edit_account_password_reset_path("blah")
+    when /my account confirmation page/i
+      edit_account_confirmation_path(User.last.confirmation_token)
+    when /an invalid account confirmation page/i
+      edit_account_confirmation_path("blah")
     else
       begin
         page_name =~ /the (.*) page/
