35b98e378bc88e881ec82e51321fdf473c93db33
Mack Earnhardt <mack@agilereasoning.com>
2011-04-04 10:38:01 -0400

    Default rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseCloser::Application.config.secret_token = '72ac3b8deb88ad63ca0caa0b21e453c338616b867e0dbb82b97b6818bc5de61c02c25fcecc7268c4262d554e222db70d25c9357e3f220019681488bb78583d9b'
